2c2382066298c57d62e7f715c4cdb2fd3a9baf81
Ashi Krishnan <me@ashi.works>
2015-11-16 19:51:03 -0500

    Fix tests.

--- test/controllers/colors_controller_test.rb
+++ test/controllers/colors_controller_test.rb
@@ -4,24 +4,24 @@ class ColorsControllerTest < ActionController::TestCase
   test "shows a single color" do
     get :show, {colors: 'Aquamarine'}
     assert_template :show
-    assert_select '.color', 'Aquamarine'
+    assert_select '.color', /Aquamarine/
   end
 
   test "shows a list of colors" do
     get :show, {colors: 'Aquamarine,Antique Brass'}
     assert_template :list
-    assert_select '.color', 'Aquamarine'
-    assert_select '.color', 'Antique Brass'
+    assert_select '.color', /Aquamarine/
+    assert_select '.color', /Antique Brass/
   end
 
   test "single colors have hex codes in their names" do
     get :show, {colors: 'Aquamarine'}
-    assert_select '.color', "Aquamarine (#{Color::Aquamarine.hex})"
+    assert_select '.color', /#{Color::Aquamarine.hex}/
   end
 
   test "colors in a list have hex codes in their names" do
     get :show, {colors: 'Aquamarine,Antique Brass'}
-    assert_select '.color', "Aquamarine (#{Color::Aquamarine.hex})"
-    assert_select '.color', "Antique Brass (#{Color::AntiqueBrass.hex})"
+    assert_select '.color', /#{Color::Aquamarine.hex}/
+    assert_select '.color', /#{Color::AntiqueBrass.hex}/
   end
 end
